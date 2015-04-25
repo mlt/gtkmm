d063cae20fb05fa8981892f923b74096c749647c
Mikhail Titov <mlt@gmx.us>
2015-04-24 22:56:06 -0500

    Allow to specify MINGW root via CLI in a2lib.py

--- a2lib.py
+++ a2lib.py
@@ -1,8 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""Convert .a to .lib by generating a list of exported functions from
+DLL into .def file and generating import library based on that .def
+file. Run this tool from VS command prompt."""
+
+import argparse
 import os,re,sys,shutil
 from os.path import join, getsize
 from subprocess import Popen, PIPE
-os.environ['PATH'] = os.environ['PATH'] + ";C:\\Program Files\\Microsoft Visual Studio 9.0\\Common7\\IDE\\;C:\\Program Files\\Microsoft Visual Studio 9.0\\VC\\bin\\;C:\\MinGW\\bin"
-root = sys.path[0] + "\\usr\\i686-w64-mingw32\\sys-root\\mingw"
 
 def gen(dll,lib,d):
     output = Popen(["nm", lib], stdout=PIPE).communicate()[0]
@@ -16,19 +22,34 @@ def gen(dll,lib,d):
                     f.write(line + b"\n")
         f.write(str.encode("LIBRARY %s\n" % dll))
 
-os.chdir(root + "\\lib")
-for root, dirs, files in os.walk(root + "\\bin"):
-    for f in files:
-        if (re.search(".dll", f)):
-            name = re.sub("^lib", "", f)
-            name = re.sub("\\.dll$", "", name)
-            name = re.sub("(?:-\\d+)$", "", name)
-            d = "%s.def" % name
-            if not os.path.exists(d):
-                print("Working on %s to produce %s\n" % (f, d))
-                lib = "lib%s.dll.a" % name
-                gen(f, lib, d)
-            Popen(["lib", "/def:%s" % d, "/name:%s" % f]).communicate()
+def walk(root):
+    os.chdir(root + "\\lib")
+    for root, dirs, files in os.walk(root + "\\bin"):
+        for f in files:
+            if (re.search(".dll", f)):
+                name = re.sub("^lib", "", f)
+                name = re.sub("\\.dll$", "", name)
+                name = re.sub("(?:-\\d+)$", "", name)
+                d = "%s.def" % name
+                if not os.path.exists(d):
+                    print("Working on %s to produce %s\n" % (f, d))
+                    lib = "lib%s.dll.a" % name
+                    gen(f, lib, d)
+                Popen(["lib", "/def:%s" % d, "/name:%s" % f]).communicate()
 
-gen("msvcrt.dll", "libmsvcrt.a", "libmsvcrt.def")
-Popen(["lib", "/def:libmsvcrt.def"]).communicate()
+    gen("msvcrt.dll", "libmsvcrt.a", "libmsvcrt.def")
+    Popen(["lib", "/def:libmsvcrt.def"]).communicate()
+
+def get_parser():
+    parser = argparse.ArgumentParser(description=__doc__,
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--root',
+                        default=sys.path[0] + "\\usr\\i686-w64-mingw32\\sys-root\\mingw",
+                        help='Root directory for MINGW. The one that has /bin and /lib.')
+    return parser
+
+
+if __name__ == '__main__':
+    p = get_parser()
+    args = p.parse_args()
+    walk(args.root)
